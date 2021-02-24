0315e0c4c832f37cb99c7c345584703197249b46
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-02-24 19:30:27 +0100

    create graph dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -40,28 +40,74 @@
                 <h1>Tableau de bord</h1>
                 <div class="container content__dashboard">
                     <div class="container__row">
-                        <div class="container__col-3">
+                        <div class="container__col-4">
 
-                            <div class="container__row module">
-                                <div class="container__col-6 module-class">Elèves</div>
-                                <div class="container__col-6 module-class">Professeurs</div>
+                            <div class="container__row module__count">
+                                <div class="container__col-6 module__class_student">
+                                    <h3>Eleves</h3>
+                                    <h3>1900</h3>
+                                </div>
+
+                                <div class="container__col-6 module-class_prof">
+                                    <h3>Professeurs</h3>
+                                    <h3>34</h3>
+                                </div>
                             </div>
 
-                            <div class="container__row module">
-                                <div class="container__col-12">Elèves par classe</div>
+                            <div class="container__row module__student">
+                                <div class="container__col-12">
+                                    <h3>Eleves par classe</h3>
+                                    <svg viewBox="0 0 32 32">
+                                        <circle r="16" cx="16" cy="16" />
+                                    </svg>
+                                    <div class="">
+
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="container__row module">
-                                <div class="container__col-12">Trafic du site</div>
+                            <div class="container__row module__traffic">
+                                <div class="container__col-12">
+                                    <table id="q-graph">
+                                        <caption>Traffic du site</caption>
+
+                                        <tbody>
+                                        <tr class="qtr" id="q1">
+                                            <th scope="row">Q1</th>
+                                            <td class="sent bar" style="height: 111px;"><p>$18,450.00</p></td>
+                                        </tr>
+                                        <tr class="qtr" id="q2">
+                                            <th scope="row">Q2</th>
+                                            <td class="sent bar" style="height: 206px;"><p>$34,340.72</p></td>
+                                        </tr>
+                                        <tr class="qtr" id="q3">
+                                            <th scope="row">Q3</th>
+                                            <td class="sent bar" style="height: 259px;"><p>$43,145.52</p></td>
+                                        </tr>
+                                        <tr class="qtr" id="q4">
+                                            <th scope="row">Q4</th>
+                                            <td class="sent bar" style="height: 110px;"><p>$18,415.96</p></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+
+                                    <div id="ticks">
+                                        <div class="tick" style="height: 59px;"><p>2500</p></div>
+                                        <div class="tick" style="height: 59px;"><p>2000</p></div>
+                                        <div class="tick" style="height: 59px;"><p>1500</p></div>
+                                        <div class="tick" style="height: 59px;"><p>1000</p></div>
+                                        <div class="tick" style="height: 59px;"><p>500</p></div>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="container__row module">
+                            <div class="container__row module__navigator">
                                 <div class="container__col-12">Top 3 des navigateurs</div>
                             </div>
 
                         </div>
 
-                        <div class="container__col-9">
+                        <div class="container__col-8">
 
                             <div class="container__row module-calendar">
                                 <div class="container__col-12">Calendrier</div>
