43366e13132ed3693d44bbad904bc9b7084769cc
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-11 12:35:46 +0200

    add pageManager controller & view

---
+++ App/Controller/Admin/AdminPageController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller\Admin;
+
+use Core\Controller;
+use Core\Http\Request;
+use Core\Http\Response;
+use App\Model\PageModel;
+use App\Query\PageQuery;
+use Core\Component\Validator;
+
+class AdminPageController extends Controller {
+
+    private $validator;
+
+    private $request;
+
+    private $response;
+
+    private $pageModel;
+
+    private $pageQuery;
+
+
+    public function __construct()
+    {
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->pageModel = new PageModel();
+        $this->pageQuery = new PageQuery();
+        $this->validator = new Validator();
+    }
+
+    public function indexPageManager()
+    {
+        $pages = $this->pageQuery->getPages();
+        $this->render("admin/page/pageManager.phtml", ['pages'=>$pages]);
+    }
+
+}
