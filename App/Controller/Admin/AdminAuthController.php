c560b082bcf2220e3d8227c26c531b0d18066b6d
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-11 11:25:24 +0200

    add datatable interface (not functionnal)

--- App/Controller/Admin/AdminAuthController.php
+++ App/Controller/Admin/AdminAuthController.php
@@ -102,7 +102,7 @@ class AdminAuthController extends Controller{
         }
     }
 
-    public function DBConnection($data)
+    /*public function DBConnection($data)
     {
         (new DotEnv(dirname(dirname(dirname(__DIR__))) . '/.env'))->load();
         if($data['db_user'] && $data['db_name'] && $data['db_host']){
@@ -111,5 +111,5 @@ class AdminAuthController extends Controller{
             putenv('DB_HOTS') = $data['db_host'];
             putenv('DB_PASSWORD') = $data['db_password'];
         }
-    }
+    }*/
 }
