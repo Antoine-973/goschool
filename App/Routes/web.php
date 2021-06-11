fe3ef2d057adaa5b758b5b02eff0436f6d729583
calintha <cal.intha77@gmail.com>
2021-06-11 09:08:04 +0200

    Fix update article and fix grid

--- App/Routes/web.php
+++ App/Routes/web.php
@@ -37,7 +37,7 @@ $route->get('/admin/article/add', ['controller' => 'AdminArticleController', 'me
 $route->get('/admin/article/delete', ['controller' => 'AdminArticleController', 'method' => 'delete']);
 
 $route->get('/admin/article/edit', ['controller' => 'AdminArticleController', 'method' => 'edit']);
-$route->post('/admin/article/update', ['controller' => 'AdminArticleController', 'method' => 'update']);
+$route->post('/admin/article/edit', ['controller' => 'AdminArticleController', 'method' => 'update']);
 
 $route->post('/admin/users/add', ['controller' => 'AdminUserController', 'method' => 'addUser']);
 
