07a0ad044fcc9c0f08c5f330145e61336b0f480b
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-14 00:05:12 +0200

    update addCategory view

--- App/Form/CategoryAddForm.php
+++ App/Form/CategoryAddForm.php
@@ -10,7 +10,7 @@ class CategoryAddForm
     public function getForm()
     {
 
-        $form = Form::create('/admin/article/add')
+        $form = Form::create('/admin/categorie/add')
             ->input('name', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'required' => 'required'])
             ->input('slug', 'text', ['value' => 'Slug', 'min' => 4, 'max' => 55])
             ->select('parent_category','Parent Category',['id' => 'parent_category', 'name' => 'parent_category', 'options' => ['Uncategorized']])
