cb051a4ca59346637b4c3ed829676c51e27e3321
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-13 20:44:23 +0200

    functionnal page editing with ckeditor

--- App/Controller/Admin/AdminPageController.php
+++ App/Controller/Admin/AdminPageController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller\Admin;
 
+use App\Form\PageAddForm;
+use App\Form\PageEditForm;
 use Core\Controller;
 use Core\Http\Request;
 use Core\Http\Response;
@@ -38,14 +40,63 @@ class AdminPageController extends Controller {
 
     public function indexAddPage()
     {
-        $pages = $this->pageQuery->getPages();
-        $this->render("admin/page/addPage.phtml");
+        $form = new PageAddForm();
+        $pageAddForm = $form->getForm();
+
+        $this->render("admin/page/addPage.phtml", ['pageAdd'=>$pageAddForm]);
+    }
+
+    public function addPage(){
+        if($this->request->isPost()) {
+            $data = $this->request->getBody();
+
+            if($this->pageQuery->create($data))
+            {
+                $this->request->redirect('/admin/pages')->with('created', 'La page a bien été créee');
+            }
+            else{
+                $this->request->redirect('/admin/pages')->with('failed', 'Une erreur c\'est produite veuillez réessayer');
+            }
+        }
     }
 
     public function indexEditPage()
     {
-        $pages = $this->pageQuery->getPages();
-        $this->render("admin/page/editPage.phtml");
+        $form = new PageEditForm();
+        $pageEditForm = $form->getForm();
+
+        $this->render("admin/page/editPage.phtml", ['pageEdit'=>$pageEditForm]);
+    }
+
+    public function editPage()
+    {
+        if($this->request->isPost()) {
+            $data = $this->request->getBody();
+            $id = $data['id'];
+            $dataToUpdate = array_slice($data, 1);
+            $errors = $this->validator->validate($this->pageModel, $dataToUpdate);
+
+            if($this->pageQuery->updatePage($dataToUpdate, $id)) {
+                $this->request->redirect('/admin/pages')->with('edited', 'La page a bien été édité');
+            }
+            else{
+                $this->request->redirect('/admin/pages')->with('failed', 'Une erreur c\'est produite veuillez réessayer');
+            }
+        }
+    }
+
+    public function deletePage()
+    {
+        $id = $_GET['id'];
+        if($this->request->isGet()) {
+            if($this->pageQuery->delete($id)) {
+                $this->request->redirect('/admin/pages')->with('deleted', 'La page a bien été supprimé');
+            } else {
+                $this->request->redirect('/admin/pages')->with('failed', 'Une erreur c\'est produite veuillez réessayer');
+            }
+        } else {
+            $this->request->redirect('/admin/pages')->with('failed', 'Une erreur c\'est produite veuillez réessayer');
+        }
     }
 
 }
