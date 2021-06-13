fca44ddeb06685c7303d5a437267500a785d90ad
Christian <Mohindi>
2021-06-13 21:42:30 +0200

    add exemple for input value

--- App/Form/ArticleAddForm.php
+++ App/Form/ArticleAddForm.php
@@ -9,9 +9,13 @@ class ArticleAddForm
 
     public function getForm()
     {
-
+        $article = [
+            'title' => 'hello world',
+            'content' => 'lorem ipsum'
+    
+    ];
         $form = Form::create('/admin/article/add')
-                ->input('title', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'required' => 'required'])
+                ->input('title', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'required' => 'required', 'text' => $article['title']])
                 ->textarea('content', 'textarea', ['max' => 400])
                 ->input('submit', 'submit', ['value' => 'Ajouter']);
         return $form->getForm();
