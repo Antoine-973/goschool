b9f359ce5cc1cb70be28e1215b6f4dfa342a475f
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-13 23:06:18 +0200

    add categoryModel

---
+++ App/Model/CategoryModel.php
@@ -0,0 +1,79 @@
+<?php
+namespace App\Model;
+use Core\Database\Model;
+
+
+class CategoryModel extends Model
+{
+    private $name;
+
+    private $slug;
+
+    private $description;
+
+    private $image;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image): void
+    {
+        $this->image = $image;
+    }
+}
