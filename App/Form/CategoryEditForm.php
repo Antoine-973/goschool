5f8066791d9100a588118c33c40382f54522b259
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-17 01:10:35 +0200

    add categoryEditForm

--- App/Form/CategoryEditForm.php
+++ App/Form/CategoryEditForm.php
@@ -18,11 +18,11 @@ class CategoryEditForm
     {
         $id = $this->request->getBody();
 
-        $form = Form::create('/admin/user/edit')
+        $form = Form::create('/admin/categorie/edit')
             ->input('id', 'hidden', ['value' => $id['id']])
             ->input('name', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'required' => 'required'])
             ->input('slug', 'text', ['value' => 'Slug', 'min' => 4, 'max' => 55])
-            ->select('parent_category','Parent Category',['id' => 'parent_category', 'name' => 'parent_category', 'options' => ['Uncategorized']])
+            ->select('categorie_parent','Catégorie Parent',['id' => 'categorie_parent', 'name' => 'categorie_parent', 'options' => ['Aucune', 'Non classé']])
             ->textarea('description', 'textarea', ['max' => 400])
             ->input('submit', 'submit', ['value' => 'Modifier']);
         return $form->getForm();
