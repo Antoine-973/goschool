8edbfd3938ebf378fbde49c531649c22d685ad75
calintha <cal.intha77@gmail.com>
2021-06-20 19:36:49 +0200

    Add dashboard template

--- App/Controller/Admin/DashBoardController.php
+++ App/Controller/Admin/DashBoardController.php
@@ -2,10 +2,28 @@
 namespace App\Controller\Admin;
 
 use Core\Controller;
+use Core\Http\Request;
+use Core\Http\Response;
+use App\Model\ArticleModel;
 
 class DashBoardController extends Controller{
 
+    private $request;
+
+    private $response;
+
+    private $articleModel;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->articleModel = new ArticleModel();
+
+    }
+
     public function index(){
-        $this->render('admin/index.phtml');
+        $articles = ($this->articleQuery->getArticles());
+        $this->render('admin/index.phtml', ['articles'=>$articles]);
     }
 }
