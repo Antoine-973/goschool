993e5680ea78d27bc082281f36bba42f86fc18fe
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-06-21 19:15:09 +0200

    add utils function for converting multidimentional array to single

--- Core/Util/Table.php
+++ Core/Util/Table.php
@@ -8,6 +8,22 @@ class Table{
         return is_array($array[array_key_first($array)]);
     }
 
+    public function multi_to_single(array $multiArray):array
+    {
+        if ($this->is_multi($multiArray)){
+            $singleArray = array();
+            foreach($multiArray as $array) {
+                foreach($array as $key=>$value) {
+                    array_push($singleArray,$value);
+                }
+            }
+            return $singleArray;
+        }
+        else{
+            return $multiArray;
+        }
+    }
+
 }
 
 
