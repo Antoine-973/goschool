c69c6c7db40fd6948ebc31b877dfd6963cff2adf
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-07-05 01:32:37 +0200

    add status in articles forms

--- App/Form/ArticleAddForm.php
+++ App/Form/ArticleAddForm.php
@@ -12,6 +12,7 @@ class ArticleAddForm
 
         $form = Form::create('/admin/article/add')
                 ->input('title', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'required' => 'required'])
+                ->select('status','Statut',['id' => 'status', 'name' => 'status', 'options' => ['Publié','Brouillon','Attente de validation']])
                 ->textarea('content', 'textarea', ['max' => 400])
                 ->input('submit', 'submit', ['value' => 'Ajouter']);
         return $form->getForm();
