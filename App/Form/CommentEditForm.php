ddb3ab9c59d29ce07bc817f902a400e9c3233812
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-07-09 19:41:32 +0200

    auto fill commentEditForm inputs

--- App/Form/CommentEditForm.php
+++ App/Form/CommentEditForm.php
@@ -4,25 +4,31 @@ namespace App\Form;
 use Core\Interfaces\FormInterface;
 use Core\Facade\Form;
 use Core\Http\Request;
+use App\Query\CommentQuery;
 
 class CommentEditForm
 {
 
     private $request;
+    private $commentQuery;
 
     public function __construct(){
         $this->request = new Request();
+        $this->commentQuery = new CommentQuery();
     }
 
     public function getForm()
     {
         $id = $this->request->getBody();
+        $stringId = implode("','",$id);
+
+        $data=$this->commentQuery->getById($stringId);
 
         $form = Form::create('/admin/comment/edit')
             ->input('id', 'hidden', ['value' => $id['id']])
-            ->input('title', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'required' => 'required'])
-            ->textarea('message', 'textarea', ['min' => 3, 'max' => 280, 'required' => 'required'])
-            ->select('status','Statut',['id' => 'status', 'name' => 'status', 'options' => ['Approuvé','En-attente','Spam']])
+            ->input('title', 'text', ['value' => 'Titre', 'min' => 4, 'max' => 55, 'text' => $data['title'], 'required' => 'required'])
+            ->textarea('message', 'textarea', ['min' => 3, 'max' => 280, 'value' => $data['message'], 'required' => 'required'])
+            ->select('status','Statut',['id' => 'status', 'name' => 'status', 'text' => $data['status'], 'options' => ['Approuvé','En-attente','Spam']])
             ->input('submit', 'submit', ['value' => 'Modifier']);
         return $form->getForm();
     }
