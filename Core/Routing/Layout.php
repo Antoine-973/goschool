f3cc76ccf5dbb3655afd119253665411459bc338
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-07-11 17:09:51 +0200

    Merge branch 'feature/appearancePage' into feature/appearance

--- Core/Routing/Layout.php
+++ Core/Routing/Layout.php
@@ -14,18 +14,34 @@ class Layout {
 
     public function getAside()
     {
-        
+        $asideFile = $this->getTemplateFolder() . "aside.phtml";
+
+        \ob_start();
+        include $this->getTemplateFolder() . "aside.phtml";
+        return \ob_get_clean();
     }
 
     public function getFooter()
     {
-        
+        $footerFile = $this->getTemplateFolder() . "footer.phtml";
+
+        \ob_start();
+        include $this->getTemplateFolder() . "footer.phtml";
+        return \ob_get_clean();
     }
 
     public function getContent()
     {
         
-        return "<h1>Hello World !</h1>";
+        return "<h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        <h1>Hello World !</h1><br><br><br><br>
+        ";
     }
 
     protected function getTemplateFolder()
