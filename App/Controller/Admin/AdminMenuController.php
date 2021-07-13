a3407bc7ec8508611df627a93ad0dc19766491a0
Antoine-973 <antoine.saunier.pro@gmail.com>
2021-07-13 02:04:25 +0200

    functionnal add menu

--- App/Controller/Admin/AdminMenuController.php
+++ App/Controller/Admin/AdminMenuController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller\Admin;
 
-use App\Form\MenuAddForm;
-use App\Form\MenuEditForm;
 use App\Form\SelectMenuForm;
 use App\Query\MenuQuery;
+use App\Query\PageQuery;
 use Core\Controller;
 use Core\Http\Request;
 use Core\Http\Response;
+use App\Query\HavePageQuery;
 
 class AdminMenuController extends Controller
 {
@@ -18,11 +18,14 @@ class AdminMenuController extends Controller
 
     private $menuQuery;
 
+    private $pageQuery;
+
     public function __construct()
     {
         $this->request = new Request();
         $this->response = new Response();
         $this->menuQuery = new MenuQuery();
+        $this->pageQuery = new PageQuery();
     }
 
     public function indexSelectMenu()
@@ -46,10 +49,39 @@ class AdminMenuController extends Controller
     }
 
     public function indexAddMenu(){
-        $form = new MenuAddForm();
-        $addMenuForm = $form->getForm();
 
-        $this->render("admin/menu/addMenu.phtml", ['addMenu' => $addMenuForm]);
+        $pages = $this->pageQuery->getTitleAndId();
+
+        $this->render("admin/menu/addMenu.phtml", ['pages'=>$pages]);
+    }
+
+    public function addMenu(){
+        if ($this->request->isPost()){
+            $data =$this->request->getBody();
+
+            $menuData = array_slice($data,0, 2);
+            $pageToAddToMenu = array_slice($data, 2);
+
+            if($this->menuQuery->create($menuData)){
+
+                $menuQueryId = new MenuQuery();
+                $idMenu = $menuQueryId->getMenuIdByName($menuData['name']);
+
+                foreach ($pageToAddToMenu as $value){
+
+                    $pageToUpdate = [
+                        'menu_id' => $idMenu['id'],
+                        'page_id' => $value
+                    ];
+
+                    $havePageQuery = new HavePageQuery();
+                    $havePageQuery->create($pageToUpdate, $value);
+                }
+                $this->request->redirect('/admin/menus')->with('success', 'Le menu a bien été créer, vous pouvez maintenant choisir son emplacement dans le site.');
+            }else{
+                $this->request->redirect('/admin/menus')->with('error', 'Une erreur c\'est produite. Veuillez réessayer.');
+            }
+        }
     }
 
     public function indexEditMenu(){
